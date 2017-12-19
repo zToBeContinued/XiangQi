804d14abaaa550b4a2c20b6fc8605058cb9d8aef
zToBeContinued <598121715@qq.com>
2017-12-19 11:44:38 +0800

    更新

--- Classes/Chess.h
+++ Classes/Chess.h
@@ -7,6 +7,7 @@ class Chess :public Sprite
 	CC_SYNTHESIZE(std::string, chessName, ChessName);   //棋子名称
 	CC_SYNTHESIZE(int, player, Player);    //哪一方的棋子,0代表红色，1代表黑色
 	CC_SYNTHESIZE(Vec2, point, Point);   //棋子在棋盘的坐标
+	CC_SYNTHESIZE(int, state, State );   //棋子状态  0 被吃了  1没被吃
 public:
 	void PositionToPoint(Vec2 position, float spaceX, float spaceY);
 	static Chess* createWithSpriteFrameName(const int player, const std::string& spriteFrameName);
